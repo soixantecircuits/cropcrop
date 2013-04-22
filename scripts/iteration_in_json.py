d49e30c0977a0634a1fd114fdcf286bbd2a33f39
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-04-22 17:28:36 +0200

    Updated ./scripts/iteration_in_json.py

--- scripts/iteration_in_json.py
+++ scripts/iteration_in_json.py
@@ -8,13 +8,15 @@ from pprint import pprint       # To do pprint, rather than pprint.pprint
 os.system("clear")              # Call Clear to clean the Terminal
 json_data=open('type.json')     # Open the desired file
 data = json.load(json_data)     # Converting desired file to something understandable by Python
-pprint(data)                    # Pretty printing our file, to be sure all works well.
+# pprint(data)                    # Pretty printing our file, to be sure all works well.
 
 
 for attribute, value in data["screens"].items(): # Listing every screens
-	print (attribute)           # Screen name
-	print (attribute, value)    # Screen informations
-
-
+	print (attribute)
+	print (data["screens"][attribute]["crop_width"])
+	print (data["screens"][attribute]["crop_height"])
+	print (data["screens"][attribute]["margin_left"])
+	print (data["screens"][attribute]["margin_top"])
+	print ("\n")
 
 json_data.close() # Not absolutely necessary in little scripts, but it's better
