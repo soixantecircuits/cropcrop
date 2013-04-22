ba33ca69ad5a4820a5f0461ec803e52fff603a1b
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-04-22 16:53:51 +0200

    Adding comments in iteration_in_json.py

---
+++ scripts/iteration_in_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.2
+import json                     # For JSON
+import os                       # To call functions
+import subprocess               # Same...
+from pprint import pprint       # To do pprint, rather than pprint.pprint
+
+
+os.system("clear")              # Call Clear to clean the Terminal
+json_data=open('type.json')     # Open the desired file
+data = json.load(json_data)     # Converting desired file to something understandable by Python
+pprint(data)                    # Pretty printing our file, to be sure all works well.
+
+
+for attribute, value in data["screens"].items(): # Listing every screens
+	print (attribute)           # Screen name
+	print (attribute, value)    # Screen informations
+
+
+
+json_data.close() # Not absolutely necessary in little scripts, but it's better
