e4d2e7ce2a3893b47641392f45f24b17ecbe78f5
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-04-24 10:45:06 +0200

    Updating cropcrop.py, now it makes a .zip file containing desired videos.

--- scripts/cropcrop.py
+++ scripts/cropcrop.py
@@ -7,16 +7,21 @@ import subprocess                          # System command calls
 import sys                                 # System command calls
 from pprint import pprint                  # To do pprint, rather than pprint.pprint
 
+
+
 ## Preparing
 os.system("clear")                         # Call Clear to clean the Terminal
 
-# System call (middle % isolated this way => "..." + "%" + "...", otherwise it f*** everything up)
-videoWidthCmd                              = "mediainfo --Inform='Video;" + "%" + "Width%' " + sys.argv[1]    # System call to get video width with MediaInfo, called for fileWidth var
-videoHeightCmd                             = "mediainfo --Inform='Video;" + "%" + "Height%' " + sys.argv[1]   # System call to get video height with MediaInfo, called for fileHeight var
+
 
 if sys.argv.__len__() == 3:
-    json_data                              = open(sys.argv[2])        # Open the desired file
-    data                                   = json.load(json_data)     # Converting desired file to something understandable by Python
+
+    # System call (middle % isolated this way => "..." + "%" + "...", otherwise it f*** everything up)
+    videoWidthCmd                              = "mediainfo --Inform='Video;" + "%" + "Width%' " + sys.argv[1]    # System call to get video width with MediaInfo, called for fileWidth var
+    videoHeightCmd                             = "mediainfo --Inform='Video;" + "%" + "Height%' " + sys.argv[1]   # System call to get video height with MediaInfo, called for fileHeight var
+
+    json_data                              = open(sys.argv[2])                      # Open the desired file
+    data                                   = json.load(json_data)                   # Converting desired file to something understandable by Python
 	# Stock informations
     videoInformations = {
         "filename"                         : sys.argv[1].split(".")[0],
@@ -25,21 +30,22 @@ if sys.argv.__len__() == 3:
         "height"                           : int(subprocess.check_output(videoHeightCmd, shell=True))
     }
 
-## Cropping
-    for value in range(len(data["screens"])):                         # Listing every screens
+    os.system("mkdir %(filename)s" % videoInformations)                              # Make the directory containing video
+
+    ## Cropping
+    for value in range(len(data["screens"])):                                        # Listing every screens
         videoInformations["screenId"]      = data["screens"][value]["name"]
         videoInformations["cropWidth"]     = data["screens"][value]["crop_width"]
         videoInformations["cropHeight"]    = data["screens"][value]["crop_height"]
         videoInformations["marginLeft"]    = data["screens"][value]["margin_left"]
         videoInformations["marginTop"]     = data["screens"][value]["margin_top"]
-        cropCommand                        = "ffmpeg -i %(filename)s.%(fileExt)s -strict experimental -r 25 -vf crop=%(cropWidth)s:%(cropHeight)s:%(marginLeft)s:%(marginTop)s -keyint_min 1 %(screenId)s_%(filename)s.%(fileExt)s" % videoInformations
-        print(cropCommand)
+        cropCommand                        = "ffmpeg -i %(filename)s.%(fileExt)s -strict experimental -r 25 -vf crop=%(cropWidth)s:%(cropHeight)s:%(marginLeft)s:%(marginTop)s -keyint_min 1 ./%(filename)s/%(screenId)s_%(filename)s.%(fileExt)s" % videoInformations
         os.system(cropCommand)
 
-    json_data.close()                      # If I understood, it is not absolutely necessary in little scripts because of Python's Garbage Collector. But it's better to conserve control.
-
+    os.system("zip -r %(filename)s.zip ./%(filename)s/" % videoInformations)         # Compressing the directory with video files in filename.zip
+    os.system("rm -R ./%(filename)s/" % videoInformations)                           # Removing the directory, now files are stored
+    json_data.close()                                                                # If I understood, it is not absolutely necessary in little scripts because of Python's Garbage Collector. But it's better to conserve control.
 
 
 # Giving the opportunity to user to check errors
 # input("         Press ENTER to quit.")
-
