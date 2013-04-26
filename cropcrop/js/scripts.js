a6136143a9a66e439f436de6e9ee43f57385ab63
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-04-26 13:39:42 +0200

    Updated scripts.js with function to updateVideoInformations with incomming object

--- cropcrop/js/scripts.js
+++ cropcrop/js/scripts.js
@@ -7,13 +7,37 @@ jQuery(function($){
 	var screensCropList = [];
 	var screensCropCount = 0; // Use to be an ID
 
+	videoInformations = {
+		imgSrc  : "thumbnails/video.jpg",
+		width   : 800,
+		height  : 600,
+		title   : "Battle de créateurs.mp4",
+		format  : "AVC Codec",
+		size    : "30mo",
+		FPS     : "25"
+	}
+
 	crops = {};
 	crops[1]={
 		width      : "60",
 		height     : "200",
-		marginLeft : "",
-		marginTop  : "",
+		marginLeft : "50",
+		marginTop  : "0",
 		color      : [255,255,0,0.5],
+	}
+	crops[2]={
+		width      : "66",
+		height     : "66",
+		marginLeft : "200",
+		marginTop  : "100",
+		color      : [255,0,255,0.5],
+	}
+	crops[3]={
+		width      : "200",
+		height     : "60",
+		marginLeft : "400",
+		marginTop  : "200",
+		color      : [0,255,255,0.5],
 	}
 
 
@@ -82,46 +106,46 @@ jQuery(function($){
 	/*                                             */
 	/***********************************************/
 
+	/* Functions list
+		$().affiche()
+		$().updateVideoInformations(  )
+		$().createScreen( parameter )
+		$().addScreen( parameter )
+	*/
+
 	//
 	// AFFICHE
 	//
 	// Called with $().affiche()
 	jQuery.fn.extend({
-	    affiche: function (SUPERBOB) {
-	        var retour = SUPERBOB;
+		affiche: function (SUPERBOB) {
+		    var retour = SUPERBOB;
 			$('#rouge').fadeIn();
-	    	return retour;
-	    }
-	});
-
-	//
-	// SHOW THUMBNAIL
-	//
-	// Called with $().afficheThumbnail( parameter )
-	jQuery.fn.extend({
-	    showThumbnail: function (thumbnailName) {
-	    	$('#videoContent').empty()
-	    	$('#videoContent').css({
-			    "background-image" : 'url("thumbnails/video.jpg")',
-			    width : '800px', 
-			    height : '600px'
-			});
-	    }
+			return retour;
+		}
 	});
 
 	//
 	// UPDATE VIDEO INFORMATIONS
 	//
-	// Called with $().afficheThumbnail( parameter )
+	// $().updateVideoInformations(  )
 	jQuery.fn.extend({
-	    updateVideoInformations: function (thumbnailName) {
-	    	$('#videoInformationsTitle').empty();
-	    	$('#videoInformationsFormat').empty();
-	    	$('#videoInformationsWidth').empty();
-	    	$('#videoInformationsHeight').empty();
-	    	$('#videoInformationsSize').empty();
-	    	$('#videoInformationsFPS').empty();
-	    }
+		updateVideoInformations: function ( infos ) {
+			$('#videoContent').empty()
+			$('#videoContent').css({
+				"background-image" : 'url("")',
+				"background-image" : infos.imgSrc,
+				width : infos.width, 
+				height : infos.height
+			});
+
+			$('#videoInformationsTitle').text(infos.title);
+			$('#videoInformationsFormat').text(infos.format);
+			$('#videoInformationsWidth').text(infos.width);
+			$('#videoInformationsHeight').text(infos.height);
+			$('#videoInformationsSize').text(infos.size);
+			$('#videoInformationsFPS').text(infos.FPS);
+		}
 	});
 
 	//
@@ -129,73 +153,73 @@ jQuery(function($){
 	//
 	// $().createScreen( parameter )
 	jQuery.fn.extend({
-	    createScreen: function (id) {
-	    	crops[id].width
-	    	
-		    $('#videoContent').append('<div id="cropNumber' + id + '"></div>');
+		createScreen: function (id) {
+			$('#videoContent').append('<div id="cropNumber' + id + '"></div>');
 
 
-		    $('#cropNumber' + id).css({
-		    	'position'           : 'absolute',
-			    "background-color"   : 'rgba(' + crops[id].color[0] + ',' + crops[id].color[1] + ',' + crops[id].color[2] + ',' + crops[id].color[3] + ')',
-			    'margin-top'         : crops[id].marginTop,
-			    'margin-left'        : crops[id].marginLeft,
-			    'width'              : crops[id].width, 
-			    'height'             : crops[id].height
+			$('#cropNumber' + id).css({
+				'position'           : 'absolute',
+				"background-color"   : 'rgba(' + crops[id].color[0] + ',' + crops[id].color[1] + ',' + crops[id].color[2] + ',' + crops[id].color[3] + ')',
+				'margin-top'         : crops[id].marginTop + "px",
+				'margin-left'        : crops[id].marginLeft + "px",
+				'width'              : crops[id].width + "px", 
+				'height'             : crops[id].height + "px"
 			});
 			// Draggable, resizable
-		    $('#cropNumber' + id).draggable().resizable();
+			$('#cropNumber' + id).draggable().resizable();
 
-	    }
+		}
 	});
 
 
 	//
 	// ADD SCREEN
 	//
-	// $().afficheThumbnail( parameter )
+	// $().addScreen( parameter )
 	jQuery.fn.extend({
-	    addScreen: function () {
-	    	var width = $('#navInputTextWidth').val();
-	    	var height = $('#navInputTextHeight').val();
+		addScreen: function () {
+			var width = $('#navInputTextWidth').val();
+			var height = $('#navInputTextHeight').val();
 
-	    	// Resetting to default, in case of inputs being red
+			// Resetting to default, in case of inputs being red
 			$('#navInputTextWidth').css({
-			    "background-color"   : '#FFF'
+				"background-color"   : '#FFF'
 			});
 			$('#navInputTextHeight').css({
-			    "background-color"   : '#FFF'
+				"background-color"   : '#FFF'
 			});
 
 
 			// Control to show user he forget to input something
-	    	if ( width == '' ){
+			if ( width == '' ){
 				$('#navInputTextWidth').css({
-				    "background-color"   : 'rgba(255,0,0,.5)'
+					"background-color"   : 'rgba(255,0,0,.5)'
 				});
-	    	}
-	    	if ( height  == '' ){
+			}
+			if ( height  == '' ){
 				$('#navInputTextHeight').css({
-				    "background-color"   : '#A00'
+					"background-color"   : '#A00'
 				});
-	    	}
+			}
 
-	    	// If all is ok
-	    	if ( (width != '') && ( height != '') ) {
-		    	console.log(width + " " + height)
-		    	$('#videoContent').append('<div id="cropNumber' + screensCropCount + '"></div>');
-		    	$('#cropNumber' + screensCropCount).css({
-		    		'position'           : 'absolute',
-				    "background-color"   : 'rgba(0,255,255,.5)',
-				    'width'              : width, 
-				    'height'             : height
+			// If all is ok
+			if ( (width != '') && ( height != '') ) {
+				console.log(width + " " + height)
+				$('#videoContent').append('<div id="cropNumber' + screensCropCount + '"></div>');
+				$('#cropNumber' + screensCropCount).css({
+					'position'           : 'relative',
+					"background-color"   : 'rgba(0,255,255,.5)',
+					'width'              : width, 
+					'height'             : height
 				});
 				// Draggable, resizable
-		    	$('#cropNumber' + screensCropCount).draggable().resizable();
-	    	};
-	    }
+				$('#cropNumber' + screensCropCount).draggable().resizable();
+			};
+		}
 	});
 
+
+// jQuery end
 });
 
 
