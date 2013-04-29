f9aaf5d50d766da505d2db009eaaf603379dc3b4
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-04-29 15:52:32 +0200

    Add annotate.js

--- cropcrop/js/scripts.js
+++ cropcrop/js/scripts.js
@@ -363,12 +363,3 @@ jQuery(function($){
 
 // jQuery end
 });
-
-
-/*
-var img = new Image();
-img.onload = function() {
-  alert(this.width + 'x' + this.height);
-}
-img.src = 'http://www.google.com/intl/en_ALL/images/logo.gif';
-*/
