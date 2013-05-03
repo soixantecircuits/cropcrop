570b951bf2d395ab196240606ab441f9fc550ede
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-05-03 13:23:26 +0200

    Integrated favicon closes #14

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		<link rel="stylesheet" media="screen" type="text/css" href="css/jquery-ui.css" />
 		<link rel="stylesheet" media="screen" type="text/css" href="css/styles.css" />
 		
-		<link rel="icon" type="image/png" href="img/favicon.ico" />
+		<link rel="icon" type="image/png" href="img/favicon_static.png" />
 
 		<title>CROP CROP</title>
 
