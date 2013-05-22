fb610e284d266a38d06dc4b38f152373a00e6c8c
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-05-22 13:14:25 +0200

    mixpanel, php, interface

--- server/php/test2.php
+++ server/php/test2.php
@@ -1,15 +1,13 @@
 <?php 
-if (isset($_POST['name'])) {
-$filename = $_POST['name'];
-
-
-$tmp = json_decode(exec("python3 scripts/thumbnails.py files/'". $filename."'"));
-$response = array(
-    		"message"=>$tmp
-       	);
-header('Content-Type: application/json');
+	if (isset($_POST['name']))
+	{
+		$filename = $_POST['name'];
+		$tmp = json_decode(exec("python3 scripts/thumbnails.py files/'". $filename."'"));
+		$response = array(
+			"message" => $tmp
+		);
+		header('Content-Type: application/json');
 		echo json_encode($response);
- }
-
+	}
 
 ?>
