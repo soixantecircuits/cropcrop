cb1427af34c3ccd95c87e5dec8bb0cf95a567853
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-05-23 11:57:55 +0200

    Begin to add exceptions in crop.php #87

--- server/php/crop.php
+++ server/php/crop.php
@@ -1,16 +1,48 @@
 <?php
+
     if(isset($_POST["json"])){
-        $person = json_encode($_POST["json"]);
-        $person = str_replace('\\', '', $person);
-     	$file = fopen('screen.json','w+');	
 
-        fwrite($file, $person);
-        fclose($file);
+        // DATA ANALYSIS
+        try {
+            if (json_decode($_POST["json"]) == null) {
+                throw new Exception('Invalid Json');
+            }
+            // Transform received information into json string
+            $jsonInformations = json_encode($_POST["json"]);
+            // Clean special caracters wich would crash the application
+            $jsonInformations = str_replace('\\', '', $jsonInformations);
+        }
+        catch (Exception $e){
+            echo ('Fatal error : '. $e->getMessage() );
+        }
 
-        $output = json_decode($person);
-       	//echo $output->title;
-        
+        // STOCK DATA IN FILE
+        try {
+            /* Open a file to write into */
+            $file = fopen('screen.json','w+');
+            // Stock our json into that file
+            fwrite($file, $jsonInformations);
+            // Close the file 
+            fclose($file);
+            // If exists screen.json
+            if ( ! file_exists('screen.json')) {
+                throw new Exception('screen.json has not been created');
+            }
+        }
+        catch (Exception $e){
+            echo ('Fatal error : '. $e->getMessage() );
+        }
+
+
+        // Output is what we send 
+        $output = json_decode($jsonInformations);
+
+
+        // Exec python script
         $resp = exec('python3 ./scripts/cropcrop.py ./files/'. $output->title ." screen.json  ". $output->title);
+
+
+        // Send back json informations
         echo $resp;
     }
 
