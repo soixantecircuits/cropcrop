c8c65528993b48601da8df8b978d07e732ee25ff
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-05-23 13:24:14 +0200

    Exceptions management improved #87

--- server/php/crop.php
+++ server/php/crop.php
@@ -1,58 +1,52 @@
 <?php
 
     if(isset($_POST["json"])){
-        // DATA ANALYSIS
-        $incommingJson = $_POST["json"];
-        // $incommingJson = '{"foo":"bar","hello":"hi"}';
         try {
+
+        // DATA ANALYSIS
+            $incommingJson = $_POST["json"];
+            // $incommingJson = '{"foo":"bar","hello":"hi"}';
+
             if (json_decode($incommingJson) == null) {
                 throw new Exception('Invalid Json');
             }
             $validJson = json_decode($incommingJson);
+
             // Transform received information into json string
             // $jsonInformations = json_encode($incommingJson);
-            // Clean special caracters wich would crash the application
-            // $jsonInformations = str_replace('\\', '', $jsonInformations);
-        }
-        catch (Exception $e){
-            echo ('Fatal error : '. $e->getMessage() ." ".$incommingJson );
-        }
 
         // STOCK DATA IN FILE
-        try {
+
             /* Open a file to write into */
-            $file = fopen('screen.json','w+');
+                $file = fopen('screen.json','w+');
             // Stock our json into that file
-            fwrite($file, $validJson);
+                fwrite($file, $validJson);
             // Close the file 
-            fclose($file);
+                fclose($file);
+            // If  screen.json
+                if ( file_get_contents ('screen.json') == '') {
+                    throw new Exception('screen.json is empty. Content : '.file_get_contents ('screen.json'));
+                }
             // If exists screen.json
-            if ( ! file_exists('screen.json')) {
-                throw new Exception('screen.json has not been created');
-            }
-        }
-        catch (Exception $e){
-            echo ('Fatal error : '. $e->getMessage() );
-        }
+                if ( ! file_exists('screen.json')) {
+                    throw new Exception('screen.json has not been created');
+                }
+            // Clean special caracters wich would crash the application
+                // $jsonInformations = str_replace('\\', '', $jsonInformations);
 
-
-        // validJson is what we send 
-        // $validJson = json_decode($jsonInformations);
-        // echo $validJson->title; // The MTV : Moving Testing Variable
-
-
-        // Exec python script
-        try{
-            $resp = exec('python3 ./scripts/cropcrop.py ./files/'. $validJson->title ." screen.json  ". $validJson->title, $output );
-            if(json_encode($output) == '[]'){
-                throw new Exception('Failed cropcrop.py, result is : '.json_encode($output));
-            }
+        // EXECUTE PYTHON CROP SCRIPT
+            // echo $validJson->title; // The MTV : Moving Testing Variable
+                $resp = exec('python3 ./scripts/cropcrop.py ./files/'. $validJson->title ." screen.json  ". $validJson->title, $output );
+                if(json_encode($output) == '[]'){
+                    throw new Exception('Failed cropcrop.py, result is : '.json_encode($output));
+                }
+            // Send back json informations
+                echo $resp;
         }
         catch (Exception $e){
             echo ('Fatal error : '. $e->getMessage());
         }
-        // Send back json informations
-        echo $resp;
+
     }
 
 ?>
