09879b1e284a6c22577a2bfa306653475cab9ff5
Sylvain Schmitt <sylvain.schmitt@syceya.com>
2013-05-23 13:34:52 +0200

    Exceptions management improved

--- server/php/crop.php
+++ server/php/crop.php
@@ -10,29 +10,28 @@
             if (json_decode($incommingJson) == null) {
                 throw new Exception('Invalid Json');
             }
+            // Transform received information into json string
             $validJson = json_decode($incommingJson);
 
-            // Transform received information into json string
-            // $jsonInformations = json_encode($incommingJson);
 
         // STOCK DATA IN FILE
 
-            /* Open a file to write into */
+            // Clean special caracters wich would crash the application
+                // $jsonReplaced = str_replace('\\', '', $jsonInformations);
+            // Open a file to write into
                 $file = fopen('screen.json','w+');
             // Stock our json into that file
-                fwrite($file, $validJson);
+                fwrite($file, "$jsonReplaced");
             // Close the file 
                 fclose($file);
             // If  screen.json
-                if ( file_get_contents ('screen.json') == '') {
-                    throw new Exception('screen.json is empty. Content : '.file_get_contents ('screen.json'));
+                if ( readfile('screen.json') == 0) {
+                    throw new Exception('screen.json is empty. Content : '.readfile('screen.json'));
                 }
             // If exists screen.json
                 if ( ! file_exists('screen.json')) {
                     throw new Exception('screen.json has not been created');
                 }
-            // Clean special caracters wich would crash the application
-                // $jsonInformations = str_replace('\\', '', $jsonInformations);
 
         // EXECUTE PYTHON CROP SCRIPT
             // echo $validJson->title; // The MTV : Moving Testing Variable
